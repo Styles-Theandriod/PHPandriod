b7953f7870b90b5a1c38a7aa3c9411ac6b97f34e
Styles-Theandriod <emmanuelajah188@gmail.com>
2024-02-19 12:00:31 +0100

    now the file is uploading

---
+++ class5.py
@@ -0,0 +1,34 @@
+
+
+# Person = {'name': 'John', 'last_name': ' Doe', 'Age': 36}
+
+# print(Person['name'])
+# print(Person)
+
+items = list()
+empty_list = []
+print(empty_list)
+
+fav_food = [ 'pounded yam', 'rice with chicken', 'Banga soup and stach', 'Beans' ]
+
+cars = ['Tesla', 'Benz', 'Mustang', 'Mustang', 23490582388, True,]
+
+countries = ['Canada', 'Sweeden', ' Portugal', 'Silicon Valley In USA',]
+print(cars[-1])
+
+
+# UnPACKING A LIST 
+
+artist = ['Chris brown', 'Psquare', 'Wiskid', 'Rema']
+first_artist, second_artist, *rest = artist
+# print(first_artist, second_artist)
+print(first_artist, second_artist)
+
+fav_football_players = ['Nwabai', 'Messi', 'Ronaldo', 'Nema', 'Osimehn']
+
+
+# print(artist[0:3])
+# print(artist[:-1])
+# print(artist[::2])
+# print(artist[-3:-1])
+print(fav_football_players[-1:-3])
