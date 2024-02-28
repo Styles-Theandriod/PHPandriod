6245cff2a0945f1b98d5868e62f6e01efdfb81ae
Styles-Theandriod <emmanuelajah188@gmail.com>
2024-02-28 11:48:29 +0100

    data services program using python

--- class5.py
+++ class5.py
@@ -25,6 +25,7 @@ first_artist, second_artist, *rest = artist
 print(first_artist, second_artist)
 
 fav_football_players = ['Nwabai', 'Messi', 'Ronaldo', 'Nema', 'Osimehn']
+fav_football_players = ['Nwabai', 'Messi', 'Ronaldo', 'Nema', 'Osimehn']
 
 
 # print(artist[0:3])
@@ -37,3 +38,147 @@ print(fav_football_players[-1:-3])
 print(fav_football_players[-1:-3])
 print(fav_football_players[-1:-3])
 print(fav_football_players[-1:-3])
+
+# changing items in a list
+phones_models = ['Iphone', 'Samsung', 'Hawaii', 'Oppor']
+
+changed_items = phones_models[3] = 'itel'
+print(changed_items)
+
+# Adding items to the list
+
+phones_models.append('infinise')
+
+print(phones_models)
+
+# Popping items in the list
+fav_drinks = ['Coca Cola', 'Bigie', 'fanta']
+
+fav_drinks.pop(0)
+fav_drinks.pop()
+
+print(fav_drinks)
+
+
+# Deleting items in the list
+brands = ['gucci', 'vasscii', 'dng', 'fendi', 'theandriod']
+del brands[0:2]
+
+print(brands)
+
+# List comprehension
+language = 'python'
+lst = [language]
+print(lst)
+
+
+# Python game creation 
+
+# checks data from thinking with user input if they match returns True
+
+
+userInput = float( input('Enter your year of birth: '))
+
+currentYear = 2024
+
+print('Your current age is: ', userInput - currentYear)
+
+
+
+import random
+
+database = []  # Make database a global variable
+print(database)
+
+def Game():
+    while True:
+        score = 0
+
+        thinking = ['education', 'coding', 'science', 'Programming', 'Fathering', 'Mothering', 'sisters']
+        computer_thinking = random.choice(thinking)
+
+        userInput = input('What am I thinking: ')
+
+        if userInput == computer_thinking:
+            print('You got my thinking 😂')
+            score += 1
+        else:
+            print('Wrong! Try again 😭')
+
+def createAccount():
+    print('#1 Create Account \t  #2 Login Account')
+
+    userInput = input('Enter a number: ')
+
+    if userInput == str(1):
+        username = input('Enter username: ')
+        password = input('Enter password: ')
+        database.append({'username': username, 'password': password})
+        Game()
+    elif userInput == str(2):
+        username = input('Enter username: ')
+        password = input('Enter password: ')
+        if any(entry['username'] == username and entry['password'] == password for entry in database):
+            Game()
+        else:
+            print('Invalid credentials. Try again.')
+
+createAccount()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import random
+
+# thinking = ['gucci', 'education', 'fendi', 'theandriod', 'dng', 'jadrolita', 'bizmarrow']
+
+
+# while True:
+
+#     random_thinking = random.choice(thinking)
+
+#     print(random_thinking)
+
+#     print('what am i thinking about')
+
+#     userInput = input('Your guess: ')
+
+#     score = 0
+
+#     if(userInput  in random_thinking):
+#         print('you got my thought 😂')
+#         score = score + 1
+#         print('your score is ' + str(score))
+#     else:
+#         print('wrong try again 😭')
+#         score = score - 1
+
+#     play_again = input('Do you want to play again? (yes/no):').lower()
+#     if(play_again != 'yes'):
+#         break;
+
+
+
+
---
+++ pythonWithSilas.py
@@ -0,0 +1,83 @@
+print('welcome to python class')
+
+# Variable naming in python 
+# snakeCase 
+# PascalCase
+# CamelCase 
+
+
+First_Name = 'emmanuel' #SNAKE CASE
+lastName = 'forest' # camel case
+NewAge = 23 # PASCAL CASE
+
+First_Name = 'onyekachi'
+print(First_Name)
+
+
+# case functions in python 
+print(First_Name.upper())
+print(First_Name.lower())
+print(First_Name.title())
+print(First_Name.capitalize())
+
+# datatypes in python
+# strings 
+# intergers
+# float 
+# set  
+# list 
+# tuple 
+# dict
+# complex 
+# boolean 
+
+setOfNumbers = {1, 2, 3}
+print(setOfNumbers)
+
+Numbers = [1, 2, 3, 4, 5, 6, 7]
+
+myTuple = ('Dangote', 'Patricia', 'darey abegapp')
+Dictionary = {'Name': 'Dangote', 'Type': 'Business'}
+myComplex = 10+1j
+hammantan = True
+
+print('this is the length', len(myTuple))
+
+""" this is another comment """
+''' this something wired '''
+
+# string new topic 
+first_text = 'this is some content'
+second_text = 'this is another content'
+
+# concatenation 
+
+newText = first_text + second_text
+print(newText)
+
+print(int('30') + 1)
+
+int 
+float 
+str 
+list 
+dict 
+set 
+complex
+
+
+word = str('i am a boy')
+
+newWord = word + ' ' + str('and also a programmer')
+
+print(newWord)
+
+sentence = 'i love tech'
+print(sentence[0:])
+
+
+
+
+
+
+
